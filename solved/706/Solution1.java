7aec1f1c7df57434d6c87944a7062446654242ba
Daeho Park <dhpark0648@gmail.com>
2020-09-07 01:58:44 +0900

    Solve Design HashMap

---
+++ solved/706/Solution1.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class MyHashMap {
+    private final int[] hashTable = new int[1000001];
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyHashMap() {
+        Arrays.fill(hashTable, -1);
+    }
+
+    /**
+     * value will always be non-negative.
+     */
+    public void put(int key, int value) {
+        hashTable[key] = value;
+    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+     */
+    public int get(int key) {
+        return hashTable[key];
+    }
+
+    /**
+     * Removes the mapping of the specified value key if this map contains a mapping for the key
+     */
+    public void remove(int key) {
+        hashTable[key] = -1;
+    }
+}
