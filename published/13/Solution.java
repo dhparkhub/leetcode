418ad137959da6da55e456494fb16fff8650cc70
Daeho Park <dhpark0648@gmail.com>
2020-09-10 14:06:29 +0900

    Publish 13. Roman to Integer

--- published/13/Solution.java
+++ published/13/Solution.java
@@ -2,51 +2,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    private final Map<Character, Integer> map = new HashMap<>();
-
-    public Solution() {
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-    }
+    private final Map<Character, Integer> map = new HashMap<>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
 
     public int romanToInt(String s) {
         int answer = 0;
         for (int i = 0; i < s.length(); i++) {
             char temp = s.charAt(i);
-            if (temp == 'I' && i + 1 < s.length()) {
-                if (s.charAt(i + 1) == 'V') {
+            if (i + 1 < s.length()) {
+                if (temp == 'I' && s.charAt(i + 1) == 'V') {
                     answer += 4;
                     i++;
                     continue;
-                }
-                if (s.charAt(i + 1) == 'X') {
+                } else if (temp == 'I' && s.charAt(i + 1) == 'X') {
                     answer += 9;
                     i++;
                     continue;
-                }
-            } else if (temp == 'X' && i + 1 < s.length()) {
-                if (s.charAt(i + 1) == 'L') {
+                } else if (temp == 'X' && s.charAt(i + 1) == 'L') {
                     answer += 40;
                     i++;
                     continue;
-                }
-                if (s.charAt(i + 1) == 'C') {
+                } else if (temp == 'X' && s.charAt(i + 1) == 'C') {
                     answer += 90;
                     i++;
                     continue;
-                }
-            } else if (temp == 'C' && i + 1 < s.length()) {
-                if (s.charAt(i + 1) == 'D') {
+                } else if (temp == 'C' && s.charAt(i + 1) == 'D') {
                     answer += 400;
                     i++;
                     continue;
-                }
-                if (s.charAt(i + 1) == 'M') {
+                } else if (temp == 'C' && s.charAt(i + 1) == 'M') {
                     answer += 900;
                     i++;
                     continue;
