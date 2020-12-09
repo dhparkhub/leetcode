228c460ff8a447564b6c22ef6c5058335d864bf2
Daeho Park <dhpark0648@gmail.com>
2020-12-09 15:22:27 +0900

    Solve 1346. Check If N and Its Double Exist

---
+++ solved/1346/Solution.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            if (i % 2 == 0 && set.contains(i / 2)) return true;
+            if (set.contains(i * 2)) return true;
+            set.add(i);
+        }
+        return false;
+    }
+}
