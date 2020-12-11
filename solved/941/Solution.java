533e53a26bd2dee068d79295a16d7d393b7af01d
Daeho Park <dhpark0648@gmail.com>
2020-12-11 09:04:04 +0900

    Solve 941. Valid Mountain Array

--- solved/941/Solution.java
+++ solved/941/Solution.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        int i = 0;
-        int j = arr.length - 1;
-        while (i < arr.length - 1 && arr[i] < arr[i + 1]) i++;
-        while (j > 0 && arr[j] < arr[j - 1]) j--;
-        return i == j && i > 0 && j < arr.length - 1;
+        if (arr.length < 3) return false;
+        int l = 0;
+        int r = arr.length - 1;
+        while (l + 1 < arr.length - 1 && arr[l] < arr[l + 1]) l++;
+        while (r - 1 >= 0 && arr[r] < arr[r - 1]) r--;
+        return l != arr.length - 1 && r != 0 && l == r;
     }
 }
