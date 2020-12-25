a185e8dbc1f3f0d26724633348f3644ee0b21e9c
Daeho Park <dhpark0648@gmail.com>
2020-12-25 12:00:12 +0900

    24. Swap Nodes in Pairs

---
+++ solved/24/Solution.java
@@ -0,0 +1,29 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null) return head;
+        if (head.next == null) return head;
+        ListNode first = head;
+        ListNode second = head.next;
+        ListNode third = head.next.next;
+        second.next = first;
+        first.next = swapPairs(third);
+        return second;
+    }
+}
