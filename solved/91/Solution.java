fd596f9ef589e768c40e6959164d4ba28cde1b4c
Daeho Park <dhpark0648@gmail.com>
2020-12-27 16:46:55 +0900

    91. Decode Ways

--- solved/91/Solution.java
+++ solved/91/Solution.java
@@ -1,42 +1,27 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
-    private final Map<Integer, Integer> cache = new HashMap<>();
-
     public int numDecodings(String s) {
-        return getNumDecodings(s.toCharArray(), 0);
+        return dp(s, 0, new Integer[s.length()]);
     }
 
-    private int getNumDecodings(char[] chArr, int index) {
-        if (index == chArr.length) {
-            return 1;
-        }
+    private int dp(String s, int index, Integer[] dp) {
+        if (index == s.length()) return 1;
+        if (dp[index] != null) return dp[index];
 
-        if (cache.containsKey(index)) {
-            return cache.get(index);
-        }
+        dp[index] = 0;
 
-        int tempNum1 = Character.getNumericValue(chArr[index]);
-        if (tempNum1 == 0) {
-            cache.put(index, 0);
-            return 0;
-        }
+        int num = s.charAt(index) - '0';
+        if (!canDecoded(num)) return dp[index];
+        dp[index] += dp(s, index + 1, dp);
 
-        int case1 = getNumDecodings(chArr, index + 1);
-        if (index == chArr.length - 1) {
-            cache.put(index, case1);
-            return case1;
-        }
+        if (index >= s.length() - 1) return dp[index];
 
-        int tempNum2 = tempNum1 * 10 + Character.getNumericValue(chArr[index + 1]);
-        if (tempNum2 == 0 || tempNum2 > 26) {
-            cache.put(index, case1);
-            return case1;
-        }
+        num = num * 10 + (s.charAt(index + 1) - '0');
+        if (!canDecoded(num)) return dp[index];
 
-        int case2 = getNumDecodings(chArr, index + 2);
-        cache.put(index, case1 + case2);
-        return case1 + case2;
+        return dp[index] += dp(s, index + 2, dp);
+    }
+
+    private boolean canDecoded(int i) {
+        return i >= 1 && i <= 26;
     }
 }
