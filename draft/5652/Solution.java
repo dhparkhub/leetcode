c16bc3f41a4e055699dcecbdcf9ee1222ea42cd5
Daeho Park <dhpark0648@gmail.com>
2021-01-11 14:28:39 +0900

    Weekly Contest 223

---
+++ draft/5652/Solution.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        swap(list, k - 1, list.size() - k);
+        for (int i = 0; i < list.size() - 1; i++) {
+            list.get(i).next = list.get(i + 1);
+        }
+        list.get(list.size() - 1).next = null;
+        return list.get(0);
+    }
+
+    private void swap(List<ListNode> list, int i, int j) {
+        ListNode temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    }
+}
